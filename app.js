a4a99e0ac8258a2640e87a778ff54d2dc54f6613
elifalinli <semraelifalinli@gmail.com>
2023-03-27 18:04:33 +0100

    resolve ticket 3

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 app.get("/api/categories", getCategories);
 
-
+app.all("*", (req, res, next) => {
+  res.status(404).send({ msg: "Page not found!" });
+});
 
 app.use((err, req, res, next) => {
-  if (err.status === 404) {
-    res.status(404).send({ msg: "404: does not exist!" });
-  } else {
+  if (err.status === 500) {
     console.log(err);
     res.status(500).send({ msg: "Ooopss! Server error!" });
   }
