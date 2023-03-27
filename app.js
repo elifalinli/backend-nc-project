04af0000421ffba1d3e7b27e3a1492539e668d3c
elifalinli <semraelifalinli@gmail.com>
2023-03-27 15:40:03 +0100

    handle 500 error

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { getCategories } = require('../be-nc-games/controllers/games.controllers');
 const app = express();
 
-app.get('/api/categories', getCategories)
+app.get('/api/categories', getCategories)  
+
+
 
 app.use((err, req, res, next) => {
-    if(err.status === 500){
-        res.status(500).send({msg: 'Ooops! Server error!'})
-    }
+    console.log(err);
+    res.status(500).send({msg: 'Ooopss! Server error!' });
 })
 
 
--- controllers/games.controllers.js
+++ controllers/games.controllers.js
@@ -3,12 +3,12 @@ const categories = require("../db/data/test-data/categories")
 const { selectCategories } = require("../models/games.models")
 
 
-exports.getCategories = (req, res) => {
+exports.getCategories = (req, res, next) => {
 selectCategories()
 .then((categories) => {
-    res.status(200).send({categories})})
-    .catch((err) => {
-        console.log(err)
-        res.status(400).send({msg: 'Invalid path!'})
-    });
+res.status(200).send({categories})
+})
+.catch((err) => {
+   next(err) 
+})
 };
