251ac063863d60f5041a7d819b454f780f613766
elifalinli <semraelifalinli@gmail.com>
2023-03-27 16:11:09 +0100

    solve error handling issues

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const { getCategories } = require('../be-nc-games/controllers/games.controllers');
+const express = require("express");
+const {
+  getCategories,
+} = require("../be-nc-games/controllers/games.controllers");
 const app = express();
 
-app.get('/api/categories', getCategories)  
+app.get("/api/categories", getCategories);
 
 
 
 app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send({ msg: "404: does not exist!" });
+  } else {
     console.log(err);
-    res.status(500).send({msg: 'Ooopss! Server error!' });
-})
+    res.status(500).send({ msg: "Ooopss! Server error!" });
+  }
+});
 
-
-module.exports = app
+module.exports = app;
